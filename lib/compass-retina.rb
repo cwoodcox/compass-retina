689be1c686455e6b3ce53a6fe68182e1bac8e62d
Corey Woodcox <corey.woodcox@gmail.com>
2012-09-06 22:46:02 -0600

    load the helpers for the sassyness

--- lib/compass-retina.rb
+++ lib/compass-retina.rb
@@ -1,5 +1,6 @@
 require "compass"
 require "compass-retina/version"
+require "compass/sass_extensions/functions/retina_images"
 
 extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 Compass::Frameworks.register('compass-retina', :path => extension_path)
